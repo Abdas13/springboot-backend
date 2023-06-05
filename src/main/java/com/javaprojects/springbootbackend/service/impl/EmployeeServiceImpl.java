7d93a0f5a6ebed34f95f6131236f4da3e8f82ed4
Abdas13 <88287139+Abdas13@users.noreply.github.com>
2023-06-05 14:41:44 -0400

    Update EmployeeServiceImpl.java
    added one commit

--- src/main/java/com/javaprojects/springbootbackend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/javaprojects/springbootbackend/service/impl/EmployeeServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+@Service // service layer
 public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeRepository employeeRepository;
     @Autowired
